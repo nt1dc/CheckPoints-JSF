61a17209295f6d34873996bcff00506832be2e9b
nt1dc <nt1dcare@gmail.com>
2022-04-20 04:36:35 +0300

    хочу сдохнуть

---
+++ src/test/java/PleaseTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class PleaseTest {
+    @Test
+    public void please(){
+        System.out.println("please");
+    }
+}
---
+++ src/test/java/Validators/OpportunityToBeInLiveTest.java
@@ -0,0 +1,49 @@
+package Validators;
+
+import OpportunityToBeInLive.OpportunityToBeInLive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class OpportunityToBeInLiveTest {
+    OpportunityToBeInLive opportunityToBeInLive = new OpportunityToBeInLive();
+
+
+//
+//    @Test
+//    public void checkDataIsValidExample() {
+//        Assertions.assertFalse(opportunityToBeInLive.checkDataIsValid(2.0, 2.0, 3.0));
+//    }
+
+    @TestFactory
+    Stream<DynamicTest> testNegativeCases() {
+        List<TestCase> caseList = Arrays.asList(
+                new TestCase(1.0, 2.0, 1.0, false),
+                new TestCase(5.0, 2.0, 3.0, false),
+                new TestCase(0.0, 0.0, 0.0, false)
+        );
+        return caseList.stream().map(
+                entry -> DynamicTest.dynamicTest(
+                        String.format("x:%s y:%s r:%s expected:%s", entry.x, entry.y, entry.r, entry.expected),
+                        () -> Assertions.assertEquals(opportunityToBeInLive.checkDataIsValid(entry.x, entry.y, entry.r), entry.expected)
+                )
+        );
+    }
+
+    @Data
+    @AllArgsConstructor
+    static
+    class TestCase {
+        private double x;
+        private double y;
+        private double r;
+        private boolean expected;
+    }
+}
