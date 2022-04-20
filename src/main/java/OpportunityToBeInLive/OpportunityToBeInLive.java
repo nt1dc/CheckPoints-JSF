5e20c0a62a479f7a39e26f947d7e8be771867cae
nt1dc <nt1dcare@gmail.com>
2022-04-20 12:35:05 +0300

    test final

--- src/main/java/OpportunityToBeInLive/OpportunityToBeInLive.java
+++ src/main/java/OpportunityToBeInLive/OpportunityToBeInLive.java
@@ -7,7 +7,7 @@ public class OpportunityToBeInLive {
     public OpportunityToBeInLive() {
     }
 
-    private final List<Double> rValues = Arrays.asList(2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.25, 3.5, 3.75, 4.0, 4.25, 4.5, 4.75, 5.0);
+    public final List<Double> rValues = Arrays.asList(2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.25, 3.5, 3.75, 4.0, 4.25, 4.5, 4.75, 5.0);
 
     public boolean checkDataIsValid(double x, double y, double r) {
         return rValues.contains(r) && (-4 <= x && x <= 4) && (-5 <= y && y <= 5);
--- src/test/java/Validators/OpportunityToBeInLiveTest.java
+++ src/test/java/Validators/OpportunityToBeInLiveTest.java
@@ -1,20 +1,19 @@
 package Validators;
 
 import OpportunityToBeInLive.OpportunityToBeInLive;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 
-class OpportunityToBeInLiveTest {
+public class OpportunityToBeInLiveTest {
     OpportunityToBeInLive opportunityToBeInLive = new OpportunityToBeInLive();
 
+
+    @Test
+    public void please() {
+        boolean res = opportunityToBeInLive.checkDataIsValid(1.0, 2.0, 3.0);
+        Assert.assertTrue(res);
+    }
 
 //
 //    @Test
@@ -22,28 +21,28 @@ class OpportunityToBeInLiveTest {
 //        Assertions.assertFalse(opportunityToBeInLive.checkDataIsValid(2.0, 2.0, 3.0));
 //    }
 
-    @TestFactory
-    Stream<DynamicTest> testNegativeCases() {
-        List<TestCase> caseList = Arrays.asList(
-                new TestCase(1.0, 2.0, 1.0, false),
-                new TestCase(5.0, 2.0, 3.0, false),
-                new TestCase(0.0, 0.0, 0.0, false)
-        );
-        return caseList.stream().map(
-                entry -> DynamicTest.dynamicTest(
-                        String.format("x:%s y:%s r:%s expected:%s", entry.x, entry.y, entry.r, entry.expected),
-                        () -> Assertions.assertEquals(opportunityToBeInLive.checkDataIsValid(entry.x, entry.y, entry.r), entry.expected)
-                )
-        );
-    }
-
-    @Data
-    @AllArgsConstructor
-    static
-    class TestCase {
-        private double x;
-        private double y;
-        private double r;
-        private boolean expected;
-    }
+//    @TestFactory
+//    Stream<DynamicTest> testNegativeCases() {
+//        List<TestCase> caseList = Arrays.asList(
+//                new TestCase(1.0, 2.0, 1.0, false),
+//                new TestCase(5.0, 2.0, 3.0, false),
+//                new TestCase(0.0, 0.0, 0.0, false)
+//        );
+//        return caseList.stream().map(
+//                entry -> DynamicTest.dynamicTest(
+//                        String.format("x:%s y:%s r:%s expected:%s", entry.x, entry.y, entry.r, entry.expected),
+//                        () -> Assertions.assertEquals(opportunityToBeInLive.checkDataIsValid(entry.x, entry.y, entry.r), entry.expected)
+//                )
+//        );
+//    }
+//
+//    @Data
+//    @AllArgsConstructor
+//    static
+//    class TestCase {
+//        private double x;
+//        private double y;
+//        private double r;
+//        private boolean expected;
+//    }
 }
